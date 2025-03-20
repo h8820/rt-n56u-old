91c31448d66855e818a5d14a8e02162d08a96c46
h8820 <101133120+h8820@users.noreply.github.com>
2025-03-20 12:34:44 +0800

    Create dl_toolchain.sh

---
+++ toolchain-mipsel/dl_toolchain.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DIR="toolchain-3.4.x"
+DL_NAME="mipsel-linux-uclibc.tar.xz"
+
+if [ -d "$DIR" ]; then
+    echo "$DIR exists!"
+    exit
+fi
+
+mkdir -p "$DIR" && \
+tar -xvf "$DL_NAME" -C "$DIR"
