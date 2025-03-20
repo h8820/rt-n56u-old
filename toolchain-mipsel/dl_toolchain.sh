d7e3cccafdd8252b8f7e4d731544e96fd0a8a0dd
h8820 <101133120+h8820@users.noreply.github.com>
2025-03-20 13:02:04 +0800

    Update dl_toolchain.sh

--- toolchain-mipsel/dl_toolchain.sh
+++ toolchain-mipsel/dl_toolchain.sh
@@ -2,11 +2,13 @@
 
 DIR="toolchain-3.4.x"
 DL_NAME="mipsel-linux-uclibc.tar.xz"
+DL_URL="https://github.com/hanwckf/padavan-toolchain/releases/download/v1.1/$DL_NAME"
 
-if [ -d "$DIR" ]; then
-    echo "$DIR exists!"
-    exit
+if [ -d $DIR ]; then
+	echo "$DIR exists!"
+	exit
 fi
 
-mkdir -p "$DIR" && \
-tar -xvf "$DL_NAME" -C "$DIR"
+curl -O -L $DL_URL && \
+mkdir -p $DIR && \
+tar -xvf $DL_NAME -C $DIR
